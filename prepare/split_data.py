6785e1216143b247ee86eaba04e4ed2de2768d9d
vovanphuc <vanphucpro123@gmail.com>
2022-03-11 09:44:59 +0700

    add split on silence

--- prepare/split_data.py
+++ prepare/split_data.py
@@ -1,11 +1,25 @@
 from pydub import AudioSegment
-from pydub.silence import split_on_silence
-#reading from audio mp3 file
-sound = AudioSegment.from_mp3("data/data_separate/output.mp3")
-# spliting audio files
-audio_chunks = split_on_silence(sound, min_silence_len=5000, silence_thresh=-40, keep_silence=True)
-#loop is used to iterate over the output list
-for i, chunk in enumerate(audio_chunks):
-   output_file = "data/data_split/chunk{0}.mp3".format(i)
-   print("Exporting file", output_file)
-   chunk.export(output_file, format="mp3")
+from pydub.silence import detect_nonsilent
+import random
+
+def split_on_silence(path_audio):
+    sound = AudioSegment.from_mp3(path_audio)
+    lst_nonsilent = detect_nonsilent(sound, min_silence_len=500, silence_thresh=-40)
+    start_time = 0
+    end_time = 0
+    i = 0
+    check = False
+    for seg in lst_nonsilent:
+        end_time = seg[1]
+        if check:
+            start_time = seg[0]
+        if end_time - start_time > 6000 or random.uniform(0, 1) > 0.95:
+            i +=1
+            sound[start_time:end_time].export(f'data/data_split/chunk_{i}.mp3', format="mp3")
+            print('write chunk ', i)
+            check = True
+        else:
+            check = False
+
+if __name__ == '__main__':
+    split_on_silence("data/data_separate/output.mp3")
